68e8e62a5afa446bd2dd08872734349bda875853
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-03-31 00:23:29 +0600

    cart info added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import './Cart.css';
 const Cart = ({cart}) => {
+    let totalPrice =0;
+    let shippingCarge =0;
+    let productQuantity = 0;
+    for(const product of cart){
+        productQuantity = productQuantity + product.quantity;
+        totalPrice = totalPrice+product.price * product.quantity;
+        shippingCarge = shippingCarge + product.shipping;
+    }
+    const tax = (totalPrice * 0.1).toFixed(2);
+    const grandTotal = (totalPrice + Number(tax))
     return (
-        <div>
-            <h3>Order Summary</h3>
-            <p>Selected Items: {cart.length}</p>
+        <div className='cart'>
+            <h3 className='order-summary'>Order Summary</h3>
+            <div>
+                <p className='additional-info'>Selected Items:{productQuantity}</p>
+                <p className='additional-info'>Total Price: $ {totalPrice}</p>
+                <p className='additional-info'>Total Shipping Charge: $ {shippingCarge}</p>
+                <p className='additional-info'>Tax: $ {tax}</p>
+                <h4 style={{fontSize: '25px'}}>Grand Total: $ {grandTotal}</h4>
+            </div>
         </div>
     );
 };
